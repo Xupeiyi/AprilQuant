285699b4505d370a9e74ecfae0b8489ba294f96b
xpy <xu_py311@outlook.com>
2021-07-01 17:03:52 +0800

    - 在add_atr_exit_signal中添加了切换合约时止损的信号.

--- backtest/signals.py
+++ backtest/signals.py
@@ -102,21 +102,18 @@ def add_chandelier_exit_signal(df, trs=0.12, lqk_width=0.1, lqk_floor=0.5):
         - lqk_width: liqka每期减小的步长。
         - lqk_floor: liqka能达到的最小值。
     """
-
-    position_direction = 0  # 当前头寸方向。1为多头，-1为空头，0为无头寸。
-
-    df['higher_after_entry'] = 0  # 开仓以来较高价
-    df['lower_after_entry'] = 0  # 开仓以来较低价
-    df['dliqpoint'] = 0  # 多头吊灯线
-    df['kliqpoint'] = 0  # 空头吊灯线
-    df['liqka'] = 1  # 止盈止损幅度乘数
-
     if not has_column(df, 'long_exit'):
         df['long_exit'] = False  # 多头平仓日期
 
     if not has_column(df, 'short_exit'):
         df['short_exit'] = False  # 空头平仓日期
 
+    df['higher_after_entry'] = 0  # 开仓以来较高价
+    df['lower_after_entry'] = 0  # 开仓以来较低价
+    df['dliqpoint'] = 0  # 多头吊灯线
+    df['kliqpoint'] = 0  # 空头吊灯线
+    df['liqka'] = 1  # 止盈止损幅度乘数
+    position_direction = 0  # 当前头寸方向。1为多头，-1为空头，0为无头寸。
     for i in range(len(df)-1):
 
         # 开多头
@@ -166,38 +163,49 @@ def add_chandelier_exit_signal(df, trs=0.12, lqk_width=0.1, lqk_floor=0.5):
 
 def add_position_direction(df):
     """
-    在df中标记开仓方向。
+    在df中标记当前ｋ线开盘时的头寸方向。1为多头，-1为空头，0为无持仓。
     """
     df['position_direction'] = 0
     for i in range(1, len(df)):
 
         # 开多头
-        if df.longgo.iloc[i] and df.position_direction.iloc[i - 1] <= 0:
-            df.position_direction.iloc[i] = 1
+        if df['longgo'].iloc[i] and df['position_direction'].iloc[i - 1] <= 0:
+            df['position_direction'].iloc[i] = 1
             continue
 
         # 开空头
-        if df.shortgo.iloc[i] and df.position_direction.iloc[i - 1] >= 0:
-            df.position_direction.iloc[i] = -1
+        if df['shortgo'].iloc[i] and df['position_direction'].iloc[i - 1] >= 0:
+            df['position_direction'].iloc[i] = -1
             continue
 
         # 平多头
-        if df.long_exit.iloc[i] and df.position_direction.iloc[i - 1] > 0:
+        if df['long_exit'].iloc[i] and df['position_direction'].iloc[i - 1] > 0:
             df.position_direction.iloc[i] = 0
             continue
 
         # 平空头
-        if df.short_exit.iloc[i] and df.position_direction.iloc[i - 1] < 0:
-            df.position_direction.iloc[i] = 0
+        if df['short_exit'].iloc[i] and df['position_direction'].iloc[i - 1] < 0:
+            df['position_direction'].iloc[i] = 0
             continue
 
-        df.position_direction.iloc[i] = df.position_direction.iloc[i - 1]
+        df['position_direction'].iloc[i] = df['position_direction'].iloc[i - 1]
 
 
-def add_atr_exit_signal(df, atr_length, trs):
+def add_atr_exit_signal(df, atr_timeperiod, trs):
+    """
+    使用ATR指标添加出场信号。
+    止损条件：1.多头合约价格低于（持仓以来最高价 - trs倍ATR）。
+            2.空头合约价格高于（持仓以来最低价 + trs倍ATR）。
+            3.切换合约。
+    params:
+        - df: 一段连续的行情数据。需要行情中包括前复权开盘价adjusted_open,
+              最高价adjusted_high, 前复权最低价adjusted_low, 前复权收盘价
+              adjusted_close，并标注开仓时刻longgo和shortgo。
+        - atr_timeperiod: ATR指标移动窗宽长度。
+        - trs: 止损幅度乘数。
+    """
     close, high, low = df.adjusted_close, df.adjusted_high, df.adjusted_low
-
-    df['atr'] = ATR(high=high, low=low, close=close, timeperiod=atr_length)
+    df['atr'] = ATR(high=high, low=low, close=close, timeperiod=atr_timeperiod)
 
     position_direction = 0
     df['highest_after_entry'] = 0  # 开仓以来最高价
@@ -212,39 +220,42 @@ def add_atr_exit_signal(df, atr_length, trs):
     for i in range(len(df)-1):
 
         # 开多头
-        if df.longgo.iloc[i] and position_direction <= 0:
+        if df['longgo'].iloc[i] and position_direction <= 0:
             position_direction = 1
-            df.lowest_after_entry.iloc[i] = low.iloc[i]
+            df['lowest_after_entry'].iloc[i] = low.iloc[i]
             continue
 
         # 开空头
-        if df.shortgo.iloc[i] and position_direction >= 0:
+        if df['shortgo'].iloc[i] and position_direction >= 0:
             position_direction = -1
-            df.highest_after_entry.iloc[i] = high.iloc[i]
+            df['highest_after_entry'].iloc[i] = high.iloc[i]
             continue
 
         # 平多头
-        if df.long_exit.iloc[i] and position_direction > 0:
+        if df['long_exit'].iloc[i] and position_direction > 0:
             position_direction = 0
             continue
 
         # 平空头
-        if df.short_exit.iloc[i] and position_direction < 0:
+        if df['short_exit'].iloc[i] and position_direction < 0:
             position_direction = 0
             continue
 
         # 有持仓时计算出场条件
         if position_direction != 0:
-
+            delta = trs * df['atr'].iloc[i]
+            # 更新有持多头以来最高价，并判断是否出场
             if position_direction > 0:
-                df.highest_after_entry.iloc[i] = max(high.iloc[i],
-                                                     df.highest_after_entry.iloc[i - 1])
+                df['highest_after_entry'].iloc[i] = max(high.iloc[i],
+                                                        df['highest_after_entry'].iloc[i - 1])
                 # 平仓日期是计算出平仓信号的后一天
-                if close.iloc[i] < df.highest_after_entry.iloc[i] - trs * df.atr.iloc[i]:
-                    df.long_exit.iloc[i + 1] = True
+                if (close.iloc[i] < df['highest_after_entry'].iloc[i] - delta) or df['next_c_chg'].iloc[i]:
+                    df['long_exit'].iloc[i + 1] = True
+
+            # 更新有持多头以来最高价，并判断是否出场
             else:
-                df.lowest_after_entry.iloc[i] = min(low.iloc[i],
-                                                    df.lowest_after_entry.iloc[i - 1])
+                df['lowest_after_entry'].iloc[i] = min(low.iloc[i],
+                                                       df['lowest_after_entry'].iloc[i - 1])
                 # 平仓日期是计算出平仓信号的后一天
-                if close.iloc[i] > df.lowest_after_entry.iloc[i] + trs * df.atr.iloc[i]:
-                    df.short_exit.iloc[i + 1] = True
+                if (close.iloc[i] > df['lowest_after_entry'].iloc[i] + delta) or df['next_c_chg'].iloc[i]:
+                    df['short_exit'].iloc[i + 1] = True
